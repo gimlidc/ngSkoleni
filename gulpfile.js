003978f015ec36dd694766aa9a9f00a57a1e040d
Honza Blazek <gimli@matfyz.cz>
2017-05-28 22:33:54 +0200

    zadani domaciho ukolu c.1

--- gulpfile.js
+++ gulpfile.js
@@ -18,3 +18,18 @@ gulp.task('webserver', ['inject'], function() {
             }
         }));
 });
+
+/*
+Domácí úkol:
+
+vytvořte gulp tasky, které:
+0) clean: smaže předchozí build adresář "target" pokud existuje (můžete využít balík gulp-clean)
+1) templates: všechny potřebné HTML šablony se uloží do jednoho js souboru "templates.js" v adresáři target (pomůže vám balík gulp-angular-templatecache)
+2) build: vytvoří concatenovaný soubor ze zdrojáků (*.js) a uloží jej do adresáře "target"
+        POZOR: Mezi soubory jsou závislosti a nelze je slepit za sebe jen tak halabala, pomůže vám balík gulp-angular-filesort
+3) vendor: vytvoří "target/vendor.js" z js souborů natahovaných z bower_components (pokuste se použít jen nutné soubory, build by měl být co nejmenší)
+4) deploy: alternativa k tasku webserver, která bude servírovat adresář "target"
+
+Mimo jiné budete potřebovat nový index.html obsažený v build adresáři využívající právě a pouze vyrobené js soubory templates.js, vendor.js a app.js.
+Jak jej vyrobíte je čistě na Vás.
+ */
